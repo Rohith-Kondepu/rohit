de0b8789cbfcd61591869c9662b708f2499d5824
RohithBabuKondepu <rohithbabukondepu@gmail.com>
2025-03-21 14:41:35 +0530

    Data changes

--- src/Component/JobCarousel.jsx
+++ src/Component/JobCarousel.jsx
@@ -1,42 +1,7 @@
 import React, { useState } from "react";
 import JobCard from "./JobCard.jsx";
 import { ChevronRight } from "lucide-react";
-import Target from "../assets/target.svg"
-import Channel from "../assets/channel.svg"
-import Volkswagen from "../assets/volkswagen.svg"
-
-const jobs = [
-  {
-    id: 1,
-    company: "Target",
-    category: "Retail",
-    type: "In Office",
-    title: "Revenue Analyst",
-    registered: 33237,
-    daysLeft: 7,
-    logo: Target,
-  },
-  {
-    id: 2,
-    company: "Volkswagen",
-    category: "Cars",
-    type: "In Office",
-    title: "Revenue Analyst",
-    registered: 33237,
-    daysLeft: 7,
-    logo: Volkswagen,
-  },
-  {
-    id: 3,
-    company: "Chanel",
-    category: "Fashion",
-    type: "In Office",
-    title: "Revenue Analyst",
-    registered: 33237,
-    daysLeft: 7,
-    logo: Channel,
-  },
-];
+import jobs from "../Data/jobs.js"
 
 const JobCarousel = () => {
   const [index, setIndex] = useState(0);
