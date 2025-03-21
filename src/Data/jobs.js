293543b25d3f303d3de25a05a511f1a6a5d57927
RohithBabuKondepu <rohithbabukondepu@gmail.com>
2025-03-21 14:41:57 +0530

    data changes

---
+++ src/Data/jobs.js
@@ -0,0 +1,37 @@
+import Target from "../assets/target.svg"
+import Channel from "../assets/channel.svg"
+import Volkswagen from "../assets/volkswagen.svg"
+
+const jobs = [
+  {
+    id: 1,
+    company: "Target",
+    category: "Retail",
+    type: "In Office",
+    title: "Revenue Analyst",
+    registered: 33237,
+    daysLeft: 7,
+    logo: Target,
+  },
+  {
+    id: 2,
+    company: "Volkswagen",
+    category: "Cars",
+    type: "In Office",
+    title: "Revenue Analyst",
+    registered: 33237,
+    daysLeft: 7,
+    logo: Volkswagen,
+  },
+  {
+    id: 3,
+    company: "Chanel",
+    category: "Fashion",
+    type: "In Office",
+    title: "Revenue Analyst",
+    registered: 33237,
+    daysLeft: 7,
+    logo: Channel,
+  },
+];
+export default jobs;
